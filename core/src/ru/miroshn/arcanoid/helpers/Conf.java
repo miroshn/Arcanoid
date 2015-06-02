91c10a739f0c0e4147c6e87cc8a64a19ee327398
miroshn <miroshn11@yandex.ru>
2015-06-02 15:56:48 +0300

    Первичные настройки проекта

---
+++ core/src/ru/miroshn/arcanoid/helpers/Conf.java
@@ -0,0 +1,12 @@
+package ru.miroshn.arcanoid.helpers;
+
+/**
+ * Класс общих настроек
+ * Created by miroshn on 02.06.15.
+ */
+public class Conf {
+    /** Ширина Экрана/Окна*/
+    public static final int DISPLAY_WIDTH = 320*2;
+    /** Высота Экрана/Окна*/
+    public static final int DISPLAY_HEIGHT = 850;
+}
--- desktop/src/ru/miroshn/arcanoid/desktop/DesktopLauncher.java
+++ desktop/src/ru/miroshn/arcanoid/desktop/DesktopLauncher.java
@@ -3,10 +3,14 @@ package ru.miroshn.arcanoid.desktop;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import ru.miroshn.arcanoid.ArcanoidGame;
+import ru.miroshn.arcanoid.helpers.Conf;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.width = Conf.DISPLAY_WIDTH;
+		config.height = Conf.DISPLAY_HEIGHT;
+		config.resizable = false;
 		new LwjglApplication(new ArcanoidGame(), config);
 	}
 }
