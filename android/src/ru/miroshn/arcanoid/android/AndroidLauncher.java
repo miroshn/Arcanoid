73b8fe52005c3b54c6ecdfcdd98533cce22c21eb
miroshn <miroshn11@yandex.ru>
2015-06-02 15:44:11 +0300

    Initial commit

---
+++ android/src/ru/miroshn/arcanoid/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package ru.miroshn.arcanoid.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import ru.miroshn.arcanoid.ArcanoidGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new ArcanoidGame(), config);
+	}
+}
---
+++ desktop/src/ru/miroshn/arcanoid/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package ru.miroshn.arcanoid.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import ru.miroshn.arcanoid.ArcanoidGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new ArcanoidGame(), config);
+	}
+}
