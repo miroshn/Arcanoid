265c1a79022e5e405dd2a5110fc08c00387fa44b
miroshn <miroshn11@yandex.ru>
2015-06-03 13:04:18 +0300

    заготовка экрана приветствия

---
+++ core/src/ru/miroshn/arcanoid/screens/WelcomeScreen.java
@@ -0,0 +1,45 @@
+package ru.miroshn.arcanoid.screens;
+
+import com.badlogic.gdx.Screen;
+
+/**
+ * Экран приветствия с названием игры <br/>
+ * Created by miroshn on 03.06.15.
+ * @author miroshn
+ */
+public class WelcomeScreen implements Screen {
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void render(float delta) {
+
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
