f57c9a0d2f5244d8bbf87e55836dd58dae8a9afe
miroshn <miroshn11@yandex.ru>
2015-06-03 14:34:06 +0300

    Рисуется заголовок игры  - он плавно появляется на экране

--- core/src/ru/miroshn/arcanoid/screens/WelcomeScreen.java
+++ core/src/ru/miroshn/arcanoid/screens/WelcomeScreen.java
@@ -4,6 +4,7 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.actions.Actions;
 import ru.miroshn.arcanoid.helpers.ui.Title;
 import ru.miroshn.arcanoid.helpers.ui.Titles;
 
@@ -16,10 +17,16 @@ public class WelcomeScreen implements Screen {
     private final Title title; // изображение названия игры
     private final Stage stage;
 
+    /**
+     * Инициализируется экран <br/>
+     * Добавляется звголовок в центр экрана, заголовок полностью прозрачен и плавно появляется
+     */
     public WelcomeScreen() {
         title = new Title(Titles.WELCOME_TITLE);
         title.setPosition((Gdx.graphics.getWidth() - title.getWidth())/2.0f
                 ,(Gdx.graphics.getHeight() - title.getHeight()/2.0f)/2.0f);
+        title.getColor().a = 0;
+        title.addAction(Actions.fadeIn(0.8f));
         stage = new Stage();
         stage.addActor(title);
     }
