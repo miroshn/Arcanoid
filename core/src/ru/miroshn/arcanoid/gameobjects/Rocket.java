3097189686f877616fffc297a8dc4af71a12fc53
miroshn <miroshn11@yandex.ru>
2015-06-04 09:09:12 +0300

    Заготовка для объекта ракетки

---
+++ core/src/ru/miroshn/arcanoid/gameobjects/Rocket.java
@@ -0,0 +1,12 @@
+package ru.miroshn.arcanoid.gameobjects;
+
+import com.badlogic.gdx.scenes.scene2d.Actor;
+
+/**
+ * Управляемая игроком ракетка
+ * <br/>Created by miroshn on 04.06.15.
+ * @author miroshn
+ */
+public class Rocket extends Actor {
+
+}
